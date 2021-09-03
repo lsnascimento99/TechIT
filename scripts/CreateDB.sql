5d493448a24fc13b52ca4c1797a69c51a3003b30
lsnascimento99 <89668587+lsnascimento99@users.noreply.github.com>
2021-09-03 13:15:01 -0300

    Alterações

--- scripts/CreateDB.sql
+++ scripts/CreateDB.sql
@@ -3,40 +3,34 @@ CREATE database temakitech;
 USE temakitech;
 
 /* INSERIDO o AUTO_INCREMENT */
-CREATE TABLE Usuarios (
+CREATE TABLE Usuario (
     Id integer primary key AUTO_INCREMENT,
     usuario varchar(30) not null,
     senha varchar(80) not null,
+    nome varchar (50),
+    sobrenome varchar(100),
+    data_nascimento date,
+    endereco varchar(80),
+    cidade varchar(40),
+    estado char(2),
     data_criacao date,
     data_modificacao date
 );
 
-CREATE TABLE Clientes (
-	id integer,
-    nome varchar (50),
-    sobrenome varchar(100),
-    idade integer,
-    endereco varchar(80) not null,
-    cidade varchar(40) not null,
-    estado char(2) not null,
-    FOREIGN KEY(id) REFERENCES usuarios(id)
-    
-);
-
 /* INSERIDO o AUTO_INCREMENT */
-create table Classificacao(
+create table Categoria(
 	id integer primary key AUTO_INCREMENT,
     nome varchar(30)
 );
 
 /* INSERIDO o AUTO_INCREMENT */
-CREATE TABLE Servicos(
+CREATE TABLE Produto(
 	id integer primary key AUTO_INCREMENT,
-    id_classificacao integer,
+    id_categoria integer,
     nome varchar(60),
     detalhe varchar(200),
     preco decimal(15,2),
-    FOREIGN KEY(id_classificacao) REFERENCES Classificacao(id)
+    FOREIGN KEY(id_categoria) REFERENCES Categoria(id)
 );
 
 /* INSERIDO o AUTO_INCREMENT */
@@ -45,16 +39,24 @@ CREATE TABLE Status_pedido (
     status varchar(30) not null
     );
 
-CREATE TABLE Pedidos (
+CREATE TABLE Pedido (
     Id integer primary key AUTO_INCREMENT,
-    id_servico integer,
+    id_auxproduto integer,
     id_usuario integer,
     quantidade integer,
     id_status integer,
     valor_total decimal(15,2),
-    FOREIGN KEY(id_servico) REFERENCES Servicos(id),
-    FOREIGN KEY(id_usuario) REFERENCES Usuarios(id),
+    FOREIGN KEY(id_usuario) REFERENCES Usuario(id),
     FOREIGN KEY(id_status) REFERENCES Status_pedido(id)
 );
+
+CREATE TABLE Aux_produto(
+	id        integer primary key auto_increment,
+	id_pedido integer ,
+	id_produto integer not null,
+    quantidade int,
+    FOREIGN KEY(id_produto) REFERENCES Produto(id),
+    FOREIGN KEY(id_pedido) REFERENCES Pedido(id)
+    );
 
 
